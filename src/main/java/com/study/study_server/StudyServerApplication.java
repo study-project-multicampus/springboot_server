d10e16e706d7e5a45542a030c1d4a44eb9a704da
jundragons <yongkari9@gmail.com>
2022-05-04 10:21:20 +0900

    Start Project

---
+++ src/main/java/com/study/study_server/StudyServerApplication.java
@@ -0,0 +1,13 @@
+package com.study.study_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/study_server/StudyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.study_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
