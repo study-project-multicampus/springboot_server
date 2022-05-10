74980b70c33b3af2a2128f6f037b47fab87560f1
minuda <min9288@gmail.com>
2022-05-10 13:53:57 +0900

    jwt 클래스 추가 완료 - 정민우

---
+++ src/main/java/com/study/study_server/controller/JwtAuthenticationController.java
@@ -0,0 +1,49 @@
+package com.study.study_server.controller;
+
+import com.study.study_server.config.JwtTokenUtil;
+import com.study.study_server.config.JwtUserDetailsService;
+import com.study.study_server.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        final Member member = userDetailService.authenticateByNameAndPassword
+                (authenticationRequest.getEmail(), authenticationRequest.getPassword());
+        final String token = jwtTokenUtil.generateToken(member.getMemberName());
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+}
+
+@Data
+class JwtRequest {
+
+    private String email;
+    private String password;
+
+}
+
+@Data
+@AllArgsConstructor
+class JwtResponse {
+
+    private String token;
+
+}
