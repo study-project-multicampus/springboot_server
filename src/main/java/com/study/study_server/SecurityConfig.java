0ccee6949f72d8b80ceb794d8f575e4df9ab38a6
jundragons <yongkari9@gmail.com>
2022-05-12 17:18:14 +0900

    Add SecurityConfig.java

---
+++ src/main/java/com/study/study_server/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.study.study_server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/**").permitAll()
+        .and().csrf().ignoringAntMatchers("/h2-console/**");
+    }
+}
