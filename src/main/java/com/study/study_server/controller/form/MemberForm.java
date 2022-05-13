fd07e74d4c873542809b3f4ec5d09f18e38564e0
minuda <min9288@gmail.com>
2022-05-13 15:43:31 +0900

    code update - 정민우

--- src/main/java/com/study/study_server/controller/form/MemberForm.java
+++ src/main/java/com/study/study_server/controller/form/MemberForm.java
@@ -2,11 +2,20 @@ package com.study.study_server.controller.form;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class MemberForm {
+
+    @NotBlank
     private String memberName;
+
+    @NotBlank
     private String memberPw;
+
+    @NotBlank
     private String memberEmail;
 }
--- src/main/java/com/study/study_server/controller/form/StudyForm.java
+++ src/main/java/com/study/study_server/controller/form/StudyForm.java
@@ -3,9 +3,15 @@ package com.study.study_server.controller.form;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter @Setter
 public class StudyForm {
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String start;
 }
