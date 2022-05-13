5648ed9a5d9f047f418f46a699c7cbc8b7171ee8
NestVail <solidcopy105@gmail.com>
2022-05-13 15:44:00 +0900

    update project

--- src/main/java/com/study/study_server/controller/form/MemberForm.java
+++ src/main/java/com/study/study_server/controller/form/MemberForm.java
@@ -3,6 +3,9 @@ package com.study.study_server.controller.form;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class MemberForm {
