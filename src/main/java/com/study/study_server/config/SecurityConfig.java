94d85337c257b2df03db38be04bed91fa5dcc4d4
minuda <min9288@gmail.com>
2022-05-13 09:59:11 +0900

    시큐리티 일부 수정 - 정민우

--- src/main/java/com/study/study_server/config/SecurityConfig.java
+++ src/main/java/com/study/study_server/config/SecurityConfig.java
@@ -2,14 +2,21 @@ package com.study.study_server.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/login/**").authenticated()
+                .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
@@ -20,5 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .deleteCookies("JSESSIONID")
                 .logoutSuccessUrl("/");
+        http.csrf()
+                .ignoringAntMatchers("/h2-console/**")
+                .disable();
+
     }
+
+
+
 }
